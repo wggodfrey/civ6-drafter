1be13dbd03a5b34ebd17822aaec04988f20e5d10
Will Godfrey <w.gowman.godfrey@gmail.com>
2019-02-06 23:33:54 -0800

    update db connection

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost/civ6drafter';
+const mongoURI = 'mongodb://player:pass@35.160.213.207/civ6drafter';
 const db = mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true });
 
 module.exports = db;
